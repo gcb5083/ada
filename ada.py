90160fd749983a337f71b3496d4ab90c45a2fef4
Geoffrey Billy <gccb5083@psu.edu>
2016-06-24 00:58:22 -0400

    Overhauled WASD conntrol so now it responds to a non~mixed mode on the motor controller

--- ada.py
+++ ada.py
@@ -1,58 +1,113 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-import os.path
-import curses
-import time
+#This script is for the Penn State Robotics Club robot named Ada
+#
+#***Please rmemeber to set the motor controller to any drive mode except for mixed channel***
+#
+#written by Geoffrey Billy
+#last updated 6/23/16
 
-controls = '\t1: Arrow Keys\t2: Trackpad\t3: Mouse\t'
+
+
+from __future__ import print_function
+import curses
+
+
+
+controls = '\t1: Arrow Keys\t2: Trackpad (broken)\t3: Mouse (unimplemented)\t'
 
 adausb = open('/dev/ttyUSB0', 'w')
 print('%rrrrrr', file=adausb)
 
 
-def openUSBPort():
-	usbport = 0
-
-	for ii in range(4, 0):
-		if os.path.isfile('dev/ttyUSB' + str(ii)):
-			usbport = ii
-			break
-
-	adausb = open('/dev/ttyUSB' + str(usbport))
 
 def controlSelection():
+
 	source = raw_input('Enter one of the following choices for the selected control scheme.' + controls)
+
 	while True:
+
 		if source == str(1):
 			arrowKeysControl()
+
 		elif source == str(2):
 			mouseTrackpadControl(0)
+
 		elif source == str(3):
 			mouseTrackpadControl(1)
+
 		else:
 			source = raw_input('Please re-enter your selection.' + controls)
 
+                source == "reset"
+
+
+
 def arrowKeysControl():
+
 	stdscr = curses.initscr()
 	curses.cbreak()
-	c = 0
-	while c != 8:
-	    c = stdscr.getch()
-	    if c == ord('w'):
-		print('!A40', file=adausb)
-	    	time.sleep(1)
-	    elif c == ord('s'):
-		print('!a40', file=adausb)
-	    	time.sleep(1)
-	    elif c == ord('a'):
-		print('!B40', file=adausb)
-	    	time.sleep(1)
-	    elif c == ord('d'):
-		print('!b40', file=adausb)
-	    	time.sleep(1)
 
-	    print('%rrrrrr', file=adausb)
+	c = 0
+        speedA = 0
+        speedB = 0
+
+	while c != ord('q'):
+
+	    c = stdscr.getch()                
+
+	    if c == ord('w'):
+                print(changeSpeed(speedA, 'A', 10), file=adausb)
+                if speedA < 100:
+                    speedA = speedA + 10
+                print(changeSpeed(speedB, 'B', 10), file=adausb)
+                if speedB < 100:
+                    speedB = speedB + 10
+ 
+	    elif c == ord('a'):
+                print(changeSpeed(speedA, 'A', 5), file=adausb)
+                if speedA < 100:
+                    speedA = speedA + 5
+                print(changeSpeed(speedB, 'B', -5), file=adausb)
+                if speedB > -100:
+                    speedB = speedB - 5
+
+	    elif c == ord('d'):
+                print(changeSpeed(speedA, 'A', -5), file=adausb)
+                if speedA > -100:
+                    speedA = speedA - 5
+                print(changeSpeed(speedB, 'B', 5), file=adausb)
+                if speedB < 100:
+                    speedB = speedB + 5
+
+	    elif c == ord('s'):
+                print(changeSpeed(speedA, 'A', -10), file=adausb)
+                if speedA > -100:
+                    speedA = speedA - 10
+                print(changeSpeed(speedB, 'B', -10), file=adausb)
+                if speedB > -100:
+                    speedB = speedB - 10
+
+            else:
+                print('%rrrrrr', file=adausb)
+
+
+
+def changeSpeed(speed, motor, increment):
+
+        speed = speed + increment
+
+        if speed >= 0:
+            return ('!' + motor + str(speed))
+
+        else:
+            if ord(motor) == ord('A'):
+                return ('!a' + str(abs(speed)))
+
+            else:
+                return ('!b' + str(abs(speed)))
+
+
 
 def mouseTrackpadControl(nn):
 	mouse = file('/dev/input/mouse' + str(nn))
@@ -75,5 +130,7 @@ def mouseTrackpadControl(nn):
 				print('!a40', file=adausb)
 		else:
 			print('%rrrrrr', file=adausb)
+
+
 
 controlSelection()
